bc61713c24135006ac55bd7de9d2a4b611349743
Florian GUILLOT <dread94@gmail.com>
2023-02-10 00:13:48 +0100

    update

--- app_python/admin.py
+++ app_python/admin.py
@@ -1,16 +1,3 @@
 from django.contrib import admin
-from .models import User, Book, Genre, BookGenre, Library, Bookseller, BookLibrary, BookUser, Group, GroupUser
-
-# Register your models here.
-admin.site.register(User)
-admin.site.register(Book)
-admin.site.register(Genre)
-admin.site.register(BookGenre)
-admin.site.register(Library)
-admin.site.register(Bookseller)
-admin.site.register(BookLibrary)
-admin.site.register(BookUser)
-admin.site.register(Group)
-admin.site.register(GroupUser)
 
 # Register your models here.
