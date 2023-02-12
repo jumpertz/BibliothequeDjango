6cc750f001692c128df56cddc4b45e38383c9421
jeremy.jumpertz@gmail.com <jeremy.jumpertz@gmail.com>
2023-02-12 07:46:44 +0100

    [ADD] ajout crud books

--- app_python/urls.py
+++ app_python/urls.py
@@ -3,10 +3,11 @@ from django.conf.urls.static import static
 from django.contrib.auth.decorators import login_required
 from django.urls import path, include
 
-from .views import index_views, user_views
+from .views import index_views, user_views, book_views
 
 urlpatterns = [
-    path('', login_required(index_views.HomePageView.as_view()), name='index'),
+    path('', book_views.Books.book_list, name='index_books'),
+    #path('', login_required(index_views.HomePageView.as_view()), name='index'),
     path('accounts/', include('django.contrib.auth.urls'), ),
     path('accounts/signup', index_views.SignUpView.as_view(), name='signup'),
     path('accounts/signin', index_views.SignInView.as_view(), name='signin'),
@@ -17,7 +18,14 @@ urlpatterns = [
     path('users/create', user_views.create_view, name='create_users'),
     path('users/update/<int:user_id>', user_views.update_view, name='update_users'),
     path('users/delete/<int:user_id>', user_views.delete, name='delete_users'),
-    path('users/upload/', user_views.upload, name='upload_users')
+    path('users/upload/', user_views.upload, name='upload_users'),
+
+    # BOOKS
+    path('books/', book_views.Books.book_list, name='index_books'),
+
+    path('books/new', book_views.Books.add, name='new_book'),
+    path('books/search', book_views.Books.search, name='search_books'),
+    path('books/delete/<int:id>/', book_views.Books.delete, name='delete_book'),
 ]
 
 if settings.DEBUG:
---
+++ app_python/views/book_views.py
@@ -0,0 +1,52 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from ..models import Book
+from ..forms.book_form import BookForm
+from django.views import generic
+from django.contrib import messages
+from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required, permission_required
+
+
+class Books(generic.TemplateView):
+
+    def __init__(self, **kwargs):
+        super().__init__(kwargs)
+        self.FILES = None
+        self.method = None
+        self.POST = None
+
+    def book_list(request):
+        books = Book.objects.all()
+        return render(request, 'books/index.html', {'books': books})
+
+    def search(request):
+        if request.method == "POST":
+            query = request.POST['search']
+            books = Book.objects.filter(title__icontains=query)
+            return render(request, 'books/search.html', {'books': books})
+        books = Book.objects.all()
+        return render(request, 'books/search.html', {'books': books})
+
+    @login_required
+    def add(self):
+        if self.method == "POST":
+            title = self.POST['title']
+            author = self.POST['author']
+            thumbnail = self.FILES.get('thumbnail', None)
+            description = self.POST['description']
+            collection = self.POST['collection']
+
+            book = Book(title=title, author=author, thumbnail=thumbnail,
+                        description=description, collection=collection)
+            book.save()
+
+            return redirect('index_books')
+
+        return render(self, 'books/new.html')
+
+    @login_required
+    def delete(self, id):
+        Book.objects.filter(pk=id).delete()
+        messages.success(self, "Le livre à bien été surprimée.")
+        return redirect('index_books')
+
